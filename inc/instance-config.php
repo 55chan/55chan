23fa0170612ab2b507159804eec9442c11cd63eb
Kureva <54978435+john-clever@users.noreply.github.com>
2020-02-13 18:23:50 -0300

    More javascript. There's also an example in instance-config.

--- inc/instance-config.php
+++ inc/instance-config.php
@@ -20,4 +20,67 @@
 	//$config['root']				= '/';
 	
 	
+
+/*
+	Javascript example
+
+	$config['additional_javascript_compile'] = true;
+	$config['additional_javascript'] = array();
+	$config['additional_javascript'][] = 'js/jquery.min.js';
+	$config['additional_javascript'][] = 'js/jquery.mixitup.min.js';
+	$config['additional_javascript'][] = 'js/jquery-ui.custom.min.js';
+	$config['additional_javascript'][] = 'js/strftime.min.js';
+	$config['additional_javascript'][] = 'js/catalog.js';
+	$config['additional_javascript'][] = 'js/captcha.js';
+	$config['additional_javascript'][] = 'js/expand.js';
+	$config['additional_javascript'][] = 'js/hide-threads.js';
+	$config['additional_javascript'][] = 'js/watch.js';
+	$config['additional_javascript'][] = 'js/options.js';
+	$config['additional_javascript'][] = 'js/style-select.js';
+	$config['additional_javascript'][] = 'js/options/general.js';
+	$config['additional_javascript'][] = 'js/post-hover.js';
+	$config['additional_javascript'][] = 'js/show-op.js';
+	$config['additional_javascript'][] = 'js/smartphone-spoiler.js';
+	$config['additional_javascript'][] = 'js/inline-expanding.js';
+	$config['additional_javascript'][] = 'js/show-backlinks.js';
+	$config['additional_javascript'][] = 'js/webm-settings.js';
+	$config['additional_javascript'][] = 'js/expand-video.js';
+	$config['additional_javascript'][] = 'js/treeview.js';
+	$config['additional_javascript'][] = 'js/expand-too-long.js';
+	$config['additional_javascript'][] = 'js/settings.js';
+	$config['additional_javascript'][] = 'js/hide-images.js';
+	$config['additional_javascript'][] = 'js/expand-all-images.js';
+	$config['additional_javascript'][] = 'js/local-time.js';
+	$config['additional_javascript'][] = 'js/auto-reload.js';
+	$config['additional_javascript'][] = 'js/options/user-css.js';
+	$config['additional_javascript'][] = 'js/options/user-js.js';
+	$config['additional_javascript'][] = 'js/forced-anon.js';
+	$config['additional_javascript'][] = 'js/toggle-locked-threads.js';
+	$config['additional_javascript'][] = 'js/toggle-images.js';
+	$config['additional_javascript'][] = 'js/mobile-style.js';
+	$config['additional_javascript'][] = 'js/no-animated-gif.js';
+	$config['additional_javascript'][] = 'js/id_highlighter.js';
+	$config['additional_javascript'][] = 'js/id_colors.js';
+	$config['additional_javascript'][] = 'js/inline.js';
+	$config['additional_javascript'][] = 'js/infinite-scroll.js';
+	$config['additional_javascript'][] = 'js/download-original.js';
+	$config['additional_javascript'][] = 'js/ajax.js';
+	$config['additional_javascript'][] = 'js/quick-reply.js';
+	$config['additional_javascript'][] = 'js/quick-post-controls.js';
+	$config['additional_javascript'][] = 'js/show-own-posts.js';
+	$config['additional_javascript'][] = 'js/youtube.js';
+	$config['additional_javascript'][] = 'js/comment-toolbar.js';
+	$config['additional_javascript'][] = 'js/catalog-search.js';
+	$config['additional_javascript'][] = 'js/catalog-updater.js';
+	$config['additional_javascript'][] = 'js/thread-stats.js';
+	$config['additional_javascript'][] = 'js/quote-selection.js';
+	$config['additional_javascript'][] = 'js/image-hover.js';
+	$config['additional_javascript'][] = 'js/auto-scroll.js';
+	$config['additional_javascript'][] = 'js/file-selector.js';
+	$config['additional_javascript'][] = 'js/gallery-view.js';
+        //$config['additional_javascript'][] = 'js/popular-boards.js';
+        //$config['additional_javascript'][] = 'js/headroom.min.js';
+	// Code tags (fix because we no longer have different scripts for each board)
+	$config['additional_javascript'][] = 'js/code_tags/run_prettify.js';
+*/
 ?>
